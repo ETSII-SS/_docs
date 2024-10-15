2caace6af4a1879196ec3f1e3143d0681e55c124
CLAUDIO ANTONIO AMAYA RODRIGUEZ <camaya@us.es>
2024-10-15 19:20:19 +0200

    Update P2ejs.cpp

--- P2/P2ejs.cpp
+++ P2/P2ejs.cpp
@@ -104,7 +104,7 @@ void TestClase() {
 
 
 /// En Java, la prueba básica de una clase similar a Crono sería
-Crono crono= new Dbg();  
+Crono crono= new Crono();  
 crono.Inicio();
 Thread.sleep(1200); // Espera 1200 ms. En C-Windows usaríamos Sleep(1200)
 float segs= crono.Lee();
